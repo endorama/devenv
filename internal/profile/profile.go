17e1f63fb83cb709d3f2c8b329c1b805206b01bd
Edoardo Tenani <edoardo.tenani@gmail.com>
2019-07-01 19:15:48 +0200

    add GenerateShellLoadFile, GenerateRunFile

--- internal/profile/profile.go
+++ internal/profile/profile.go
@@ -49,6 +49,9 @@ type Profile struct {
 	Location string
 	Plugins []string
 	Shell string
+
+	shellLoader string
+	runLoader string
 }
 
 func NewProfile(name string) (p *Profile, err error) {
@@ -58,6 +61,9 @@ func NewProfile(name string) (p *Profile, err error) {
 
 	p.Plugins = []string{"aws", "bin", "email", "envs", "gpg", "ssh", "shellhistory"}
 	p.Shell = os.Getenv("SHELL")
+
+	p.shellLoader = filepath.Join(p.Location, "load.sh")
+	p.runLoader = filepath.Join(p.Location, "run.sh")
 	
 	return p, err
 }
@@ -84,42 +90,69 @@ func (p Profile) Rehash(ctx context.Context) (err error) {
 	ui := ctx.Value("ui").(*cli.BasicUi)
 
 	ui.Info("Generating shell load file")
-	err = p.GenerateShellLoadFile()
+	file, err := p.GenerateShellLoadFile()
 	if err != nil {
 		return errors.Wrap(err, "error generating profile shell load file")
 	}
+	fmt.Println(file.String())
+	ui.Info("Save shell load file")
+	err = persistFile(p.shellLoader, file.String())
+	if err != nil {
+		ui.Error(err.Error())
+		return err
+	}
 	
 	ui.Info("Generating run file")
-	err = p.GenerateRunFile()
+	file, err = p.GenerateRunFile()
 	if err != nil {
 		return errors.Wrap(err, "error generating profile run file")
+	}
+	fmt.Println(file.String())
+	ui.Info("Save run file")
+	err = persistFile(p.runLoader, file.String())
+	if err != nil {
+		ui.Error(err.Error())
+		return err
 	}
 
 	return nil
 }
 
-func (p Profile) GenerateShellLoadFile() error {
-	var b strings.Builder
-	
-	tmpl, err := template.New("header").
+func (p Profile) GenerateShellLoadFile() (b strings.Builder, err error) {
+	tmpl, err := template.New("shell-loader").
 		Parse(shellLoaderTemplate)
-	if err != nil { panic(err) }
+	if err != nil {
+		return b, errors.Wrap(err, "cannot parse template")
+	}
 	err = tmpl.Execute(&b, p)
-	if err != nil { panic(err) }
+	if err != nil {
+		return b, errors.Wrap(err, "cannot execute template")
+	}
 	
-	fmt.Println(b.String())
-	return nil
+	return b, nil
 }
 
-func (p Profile) GenerateRunFile() error {
-	var b strings.Builder
-	
-	tmpl, err := template.New("header").
+func (p Profile) GenerateRunFile() (b strings.Builder, err error) {
+	tmpl, err := template.New("runner").
 		Parse(runTemplate)
-	if err != nil { panic(err) }
+	if err != nil {
+		return b, errors.Wrap(err, "cannot parse template")
+	}
 	err = tmpl.Execute(&b, p)
-	if err != nil { panic(err) }
+	if err != nil {
+		return b, errors.Wrap(err, "cannot execute template")
+	}
 	
-	fmt.Println(b.String())
+	return b, nil
+}
+
+func persistFile(path, content string) error {
+	file, err := os.Create(path)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+	file.WriteString(content)
 	return nil
 }
